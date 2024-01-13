24694a6c1b76f7669af774755c72e6228dc0a6a0
adi8900 <adrianszymanski242@gmail.com>
2024-01-13 22:52:03 +0100

    sm8150-common: Patch blobs to use full protobuf
    grep -r libprotobuf-cpp-lite-3.9.1.so
    grep: sm8150-common/proprietary/vendor/lib64/mediadrm/libwvdrmengine.so: binary file matches

--- extract-files.sh
+++ extract-files.sh
@@ -65,6 +65,10 @@ function blob_fixup() {
     vendor/lib64/libdlbdsservice.so | vendor/lib64/libstagefright_soft_ac4dec.so | vendor/lib64/libstagefrightdolby.so)
         "${PATCHELF}" --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
     ;;
+    # Use libprotobuf-cpp-full-3.9.1
+    vendor/lib64/mediadrm/libwvdrmengine.so
+        "${PATCHELF} --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
+    ;;
     esac
 }
 
